4c5ae44da7c8b02635617610b852c9d2bd30f75e
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-22 11:21:27 +0200

    Add vercel to the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  adapter: vercel(),
 });
